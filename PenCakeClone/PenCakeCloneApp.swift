f2bb1e54f4dcb6ca1bbc9f55e83d1e5dfc96f90d
valselee <sollleky72@gmail.com>
2023-03-18 13:18:22 +0900

    Initial Commit

---
+++ PenCakeClone/PenCakeCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenCakeCloneApp.swift
+//  PenCakeClone
+//
+//  Created by Celan on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct PenCakeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
